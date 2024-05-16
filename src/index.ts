c81aeea06017a1a79c2404c778df46809be2565b
ndpniraj <ndpniraj@gmail.com>
2024-05-16 10:24:51 +0530

    middleware functions

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ import authRouter from "./routes/auth";
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   req.on("data", (chunk) => {
+//     req.body = JSON.parse(chunk);
+//     next();
+//   });
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/auth", authRouter);
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  res.json({});
+});
 
 const port = process.env.PORT || 8989;
 
