5b4dfbff5b46960a0b0fbb151455ac7245d81886
ndpniraj <ndpniraj@gmail.com>
2024-05-17 18:24:10 +0530

    random token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,8 +1,20 @@
 import { Request, Response, RequestHandler } from "express";
+import crypto from "crypto";
 
 export const generateAuthLink: RequestHandler = (req, res) => {
   // Generate authentication link
   // and send that link to the users email address
+
+  /*
+    1. Generate Unique token for every users
+    2. Store that token securely inside the database
+       so that we can validate it in future.
+    3. Create a link which include that secure token and user information
+    4. Send that link to users email address.
+    5. Notify user to look inside the email to get the login link
+  */
+
+  const randomToken = crypto.randomBytes(36).toString("hex");
 
   console.log(req.body);
 
