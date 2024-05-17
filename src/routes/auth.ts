d884aa0ec95c048da1246363f133a903d01ce9d0
ndpniraj <ndpniraj@gmail.com>
2024-05-17 16:21:28 +0530

    validating with middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,21 @@ import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post("/generate-link", generateAuthLink);
+authRouter.post(
+  "/generate-link",
+  (req, res, next) => {
+    const { email } = req.body;
+    const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+
+    if (!regex.test(email)) {
+      // sending error response
+      return res.json({ error: "Invalid email!" });
+    }
+
+    // john@email.com
+    next();
+  },
+  generateAuthLink
+);
 
 export default authRouter;
