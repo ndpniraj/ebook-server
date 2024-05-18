8943d296f97f03cf75fa3cfa952ff10e515c0fd5
ndpniraj <ndpniraj@gmail.com>
2024-05-18 16:01:46 +0530

    auth token with jwt

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -5,6 +5,7 @@ import VerificationTokenModel from "@/models/verificationToken";
 import UserModel from "@/models/user";
 import mail from "@/utils/mail";
 import { sendErrorResponse } from "@/utils/helper";
+import jwt from "jsonwebtoken";
 
 export const generateAuthLink: RequestHandler = async (req, res) => {
   // Generate authentication link
@@ -80,6 +81,11 @@ export const verifyAuthToken: RequestHandler = async (req, res) => {
   await VerificationTokenModel.findByIdAndDelete(verificationToken._id);
 
   // TODO: authentication
+  const payload = { userId: user._id };
 
-  res.json({});
+  const authToken = jwt.sign(payload, process.env.JWT_SECRET!, {
+    expiresIn: "15d",
+  });
+
+  res.json({ authToken });
 };
