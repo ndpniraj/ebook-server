6f630587a1c9c0d0c2d1d170deba94f33d3c1021
ndpniraj <ndpniraj@gmail.com>
2024-06-16 18:33:25 +0530

    review model added

---
+++ src/models/review.ts
@@ -0,0 +1,35 @@
+import { Model, ObjectId, Schema, model } from "mongoose";
+
+interface ReviewDoc {
+  user: ObjectId;
+  book: ObjectId;
+  rating: number;
+  content?: string;
+}
+
+const reviewSchema = new Schema<ReviewDoc>(
+  {
+    user: {
+      type: Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    book: {
+      type: Schema.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ReviewModel = model("Review", reviewSchema);
+export default ReviewModel as Model<ReviewDoc>;
