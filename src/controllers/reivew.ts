e7ab4eccdaca56fc027eecf64e02542da7389926
ndpniraj <ndpniraj@gmail.com>
2024-06-16 19:00:01 +0530

    add review controller

--- src/controllers/reivew.ts
+++ src/controllers/reivew.ts
@@ -1,3 +1,16 @@
+import ReviewModel from "@/models/review";
 import { AddReviewRequestHandler } from "@/types";
 
-export const addReview: AddReviewRequestHandler = (req, res) => {};
+export const addReview: AddReviewRequestHandler = async (req, res) => {
+  const { bookId, rating, content } = req.body;
+
+  await ReviewModel.findOneAndUpdate(
+    { book: bookId, user: req.user.id },
+    { content, rating },
+    { upsert: true }
+  );
+
+  res.json({
+    message: "Review added.",
+  });
+};
